c9c38b80882853804e3a1528e0eee33a89781541
koncpt-hash <konrad.kloch@gmail.com>
2026-05-10 10:39:01 +0200

    Set temperature to 0.3 for more consistent responses

--- api/chat.js
+++ api/chat.js
@@ -19,6 +19,7 @@ module.exports = async function handler(req, res) {
       body: JSON.stringify({
         model: 'claude-haiku-4-5-20251001',
         max_tokens: 1000,
+        temperature: 0.3,
         system,
         messages,
       }),
